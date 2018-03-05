d099cf8717f442ea58118201cd76fbbc052bd113
杨良正 <tlylz99@live.cn>
2018-03-05 12:51:03 +0800

    [I]Initialize solution

---
+++ HandSchool/HandSchool/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using HandSchool.Models;
+
+namespace HandSchool.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
