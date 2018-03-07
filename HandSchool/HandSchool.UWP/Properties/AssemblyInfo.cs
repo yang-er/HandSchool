8dfbfd782376536ad064c9ce38bc7f41ab9afbb1
杨良正 <tlylz99@live.cn>
2018-03-07 14:10:36 +0800

    [F]客户端名称

--- HandSchool/HandSchool.UWP/Properties/AssemblyInfo.cs
+++ HandSchool/HandSchool.UWP/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("HandSchool.UWP")]
+[assembly: AssemblyTitle("掌上校园")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("HandSchool.UWP")]
-[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyCompany("小羊")]
+[assembly: AssemblyProduct("HandSchool")]
+[assembly: AssemblyCopyright("版权所有 © 小羊 2018。")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
