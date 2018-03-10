c9ac3ff473ba6cf07ce15835fa79f3f05f5a1c86
杨良正 <tlylz99@live.cn>
2018-03-10 11:56:25 +0800

    [F]关于页面

--- HandSchool/HandSchool/Views/AboutPage.xaml.cs
+++ HandSchool/HandSchool/Views/AboutPage.xaml.cs
@@ -12,13 +12,22 @@ namespace HandSchool.Views
 		{
 			InitializeComponent();
             Version.Text = GetType().Assembly.GetName().Version.ToString();
-            BindingContext = this;
             foreach (string title in (Application.Current as App).Support.Keys)
             {
                 SupportedSchools.Text += title + "、";
             }
             SupportedSchools.Text = SupportedSchools.Text.Trim('、');
             if (SupportedSchools.Text == "") SupportedSchools.Text = "无";
+        }
+
+        private void Web1_Clicked(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("https://github.com/yang-er"));
+        }
+        
+        private void Web2_Clicked(object sender, EventArgs e)
+        {
+            Device.OpenUri(new Uri("http://www.90yang.com"));
         }
     }
 }
