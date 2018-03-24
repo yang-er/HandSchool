0b839adc0623b44b31d1d1a5709e8179051810e0
杨良正 <tlylz99@live.cn>
2018-03-24 23:14:06 +0800

    [A]HybridWebView

---
+++ HandSchool/HandSchool/Internal/HybridWebView.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+/**
+ Thanks to https://docs.microsoft.com/en-us/xamarin/xamarin-forms/app-fundamentals/custom-renderer/hybridwebview
+ */
+
+namespace HandSchool.Views
+{
+    class HybridWebView : View
+    {
+        Action<string> action;
+        public static readonly BindableProperty UriProperty = BindableProperty.Create(
+          propertyName: "Uri",
+          returnType: typeof(string),
+          declaringType: typeof(HybridWebView),
+          defaultValue: default(string));
+
+        public string Uri
+        {
+            get { return (string)GetValue(UriProperty); }
+            set { SetValue(UriProperty, value); }
+        }
+
+        public void RegisterAction(Action<string> callback)
+        {
+            action = callback;
+        }
+
+        public void Cleanup()
+        {
+            action = null;
+        }
+
+        public void InvokeAction(string data)
+        {
+            if (action == null || data == null)
+            {
+                return;
+            }
+            action.Invoke(data);
+        }
+    }
+}
