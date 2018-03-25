cfd49ed4ad59f5dc860de742fdc1bb4001f67653
杨良正 <tlylz99@live.cn>
2018-03-25 08:09:32 +0800

    [F]ActionMode Height

--- HandSchool/HandSchool.Android/Resources/Resource.designer.cs
+++ HandSchool/HandSchool.Android/Resources/Resource.designer.cs
@@ -1708,6 +1708,9 @@ namespace HandSchool.Droid
 			// aapt resource value: 0x7f070094
 			public const int action_bar_default_height_material_overlay = 2131165332;
 			
+			// aapt resource value: 0x7f070095
+			public const int action_mode_default_height_material_overlay = 2131165333;
+			
 			// aapt resource value: 0x7f070009
 			public const int cardview_compat_inset_shadow = 2131165193;
 			
@@ -3854,8 +3857,8 @@ namespace HandSchool.Droid
 			// aapt resource value: 0x7f0b0170
 			public const int Animation_Design_BottomSheetDialog = 2131427696;
 			
-			// aapt resource value: 0x7f0b018b
-			public const int AppCompatDialogStyle = 2131427723;
+			// aapt resource value: 0x7f0b018c
+			public const int AppCompatDialogStyle = 2131427724;
 			
 			// aapt resource value: 0x7f0b00b2
 			public const int Base_AlertDialog_AppCompat = 2131427506;
@@ -4331,11 +4334,14 @@ namespace HandSchool.Droid
 			// aapt resource value: 0x7f0b000e
 			public const int CardView_Light = 2131427342;
 			
-			// aapt resource value: 0x7f0b0189
-			public const int MainTheme = 2131427721;
-			
 			// aapt resource value: 0x7f0b018a
-			public const int MainTheme_Base = 2131427722;
+			public const int MainTheme = 2131427722;
+			
+			// aapt resource value: 0x7f0b0189
+			public const int MainTheme_ActionMode = 2131427721;
+			
+			// aapt resource value: 0x7f0b018b
+			public const int MainTheme_Base = 2131427723;
 			
 			// aapt resource value: 0x7f0b002d
 			public const int Platform_AppCompat = 2131427373;
@@ -4739,8 +4745,8 @@ namespace HandSchool.Droid
 			// aapt resource value: 0x7f0b0006
 			public const int Theme_MediaRouter_LightControlPanel = 2131427334;
 			
-			// aapt resource value: 0x7f0b018c
-			public const int Theme_Splash = 2131427724;
+			// aapt resource value: 0x7f0b018d
+			public const int Theme_Splash = 2131427725;
 			
 			// aapt resource value: 0x7f0b011f
 			public const int ThemeOverlay_AppCompat = 2131427615;
