f37de0c235d117c161bfe4e43a6a26ed788fb338
杨良正 <tlylz99@live.cn>
2018-04-02 15:52:55 +0800

    [F]预防针

--- HandSchool/HandSchool/Internal/PopContentPage.cs
+++ HandSchool/HandSchool/Internal/PopContentPage.cs
@@ -7,9 +7,12 @@ namespace HandSchool.Views
     // Thanks to 山宏岳
     public class PopContentPage : ContentPage
 	{
+        private bool _destoried;
+        private NavigationPage _navpg;
+
         public bool IsModal { get; set; } = false;
-        
         private Task ContinueTask { get; } = new Task(() => { });
+        public event Action Destorying;
 
         public Task ShowAsync(INavigation navigation = null)
         {
@@ -27,6 +30,10 @@ namespace HandSchool.Views
                     _navpg = Parent as NavigationPage;
                     _navpg.Popped += Page_Popped;
                 }
+                else
+                {
+                    throw new NotSupportedException("This kind of access");
+                }
             }
             return ContinueTask;
         }
@@ -38,31 +45,26 @@ namespace HandSchool.Views
             else
                 await Navigation.PopAsync();
         }
-
-        private bool _destoried;
-
-        private NavigationPage _navpg;
-
+        
         private void Page_Disappearing(object sender, EventArgs e)
         {
-            if (_destoried)
-            {
-                return;
-            }
+            if (_destoried) return;
             _destoried = true;
+
             Disappearing -= Page_Disappearing;
+            Destorying.Invoke();
 
             ContinueTask?.Start();
         }
 
         private void Page_Popped(object sender, NavigationEventArgs e)
         {
-            if (_destoried)
-            {
-                return;
-            }
+            if (_destoried) return;
             _destoried = true;
+            
             _navpg.Popped -= Page_Popped;
+            Destorying.Invoke();
+
             ContinueTask?.Start();
         }
     }
