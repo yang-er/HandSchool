9964e2b16faaf63e47cdcd32f8e0fbb422e34556
小羊 <tlylz99@live.cn>
2018-04-11 21:24:27 +0800

    [B]new branch

--- HandSchool/HandSchool.UWP/MainPage.cs
+++ HandSchool/HandSchool.UWP/MainPage.cs
@@ -6,18 +6,16 @@ using Xamarin.Forms.Xaml;
 
 namespace HandSchool.Views
 {
-	[XamlCompilation(XamlCompilationOptions.Compile)]
-	public partial class MainPage : MasterDetailPage
+    public class MainPage : MasterDetailPage
     {
-		public MainPage()
-		{
-			InitializeComponent();
+        public OutlinePage Outline = new OutlinePage();
 
+        public MainPage()
+        {
+            Master = Outline;
+            MasterBehavior = MasterBehavior.Default;
             Outline.PrimaryListView.ItemTapped += MasterPageItemTapped;
             Outline.SecondaryListView.ItemTapped += MasterPageItemTapped;
-
-            MasterBehavior = MasterBehavior.SplitOnLandscape;
-
             Detail = NavigationViewModel.Instance.PrimaryItems[0].DestPage;
         }
 
@@ -25,28 +23,24 @@ namespace HandSchool.Views
         {
             base.OnAppearing();
 
-            if(App.Current.Service.NeedLogin && !App.Current.Service.IsLogin)
+            if (App.Current.Service.NeedLogin && !App.Current.Service.IsLogin)
             {
                 LoginViewModel.RequestAsync(App.Current.Service);
             }
         }
 
-        private async void MasterPageItemTapped(object sender, ItemTappedEventArgs e)
+        private void MasterPageItemTapped(object sender, ItemTappedEventArgs e)
         {
             if (e.Item is MasterPageItem item)
             {
                 Detail = item.DestPage;
-                
+
                 (sender as ListView).SelectedItem = null;
 
                 NavigationViewModel.Instance.PrimaryItems.ForEach((one) => one.Selected = false);
                 NavigationViewModel.Instance.SecondaryItems.ForEach((one) => one.Selected = false);
-                
-                item.Selected = true;
 
-                // Funny fucky question: why this makes fluency?
-                await Task.Delay(200);
-                // IsPresented = false;
+                item.Selected = true;
             }
         }
     }
