5472985c4c4277dbfe9a6144ddcc9792c39fbaed
杨良正 <tlylz99@live.cn>
2018-04-24 09:11:05 +0800

    [F]把牙膏挤回来

--- HandSchool/HandSchool/Views/IndexPage.xaml.cs
+++ HandSchool/HandSchool/Views/IndexPage.xaml.cs
@@ -24,10 +24,8 @@ namespace HandSchool.Views
             if (IsBusy) return;
 
             IsBusy = true;
-            var loading = Internal.Helper.ShowLoadingAlert("正在登录……");
             await Core.App.Service.RequestLogin();
             IsBusy = false;
-            loading.Invoke();
         }
     }
 }
