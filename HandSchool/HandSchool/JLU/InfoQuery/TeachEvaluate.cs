6689bf0539f99bdf77218a427fb66658aa8bf9f7
小羊 <tlylz99@live.cn>
2018-06-08 21:09:31 +0800

    [A]一键教评初步

---
+++ HandSchool/HandSchool/JLU/InfoQuery/TeachEvaluate.cs
@@ -0,0 +1,59 @@
+﻿using HandSchool.Internal;
+using HandSchool.Internal.HtmlObject;
+using HandSchool.JLU.JsonObject;
+using HandSchool.Models;
+using HandSchool.Services;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using static HandSchool.Internal.Helper;
+
+namespace HandSchool.JLU.InfoQuery
+{
+    class TeachEvaluate : IInfoEntrance
+    {
+        const string evalList = "{\"tag\":\"student@evalItem\",\"branch\":\"self\",\"params\":{\"blank\":\"Y\"}}";
+
+        public string Description => throw new NotImplementedException();
+
+        public List<string> TableHeader { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Bootstrap HtmlDocument { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public IViewResponse Binding { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public Action<string> Evaluate { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public List<InfoEntranceMenu> Menu { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public string Name => throw new NotImplementedException();
+
+        public string ScriptFileUri => throw new NotImplementedException();
+
+        public bool IsPost => throw new NotImplementedException();
+
+        public string PostValue => throw new NotImplementedException();
+
+        public string StorageFile => throw new NotImplementedException();
+
+        public string LastReport { get; private set; }
+
+        public async Task Execute()
+        {
+            LastReport = await Core.App.Service.Post("service/res.do", evalList);
+            var fetched_list = JSON<RootObject<StudEval>>(LastReport);
+            foreach (var op in fetched_list.value)
+            {
+                if (op.evalActTime.evalGuideline.evalGuidelineId == "120")
+                {
+
+                }
+            }
+
+            throw new NotImplementedException();
+        }
+
+        public void Parse() { }
+
+        public void Receive(string data)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- HandSchool/HandSchool/JLU/JSONs.cs
+++ HandSchool/HandSchool/JLU/JSONs.cs
@@ -244,7 +244,7 @@ namespace HandSchool.JLU.JsonObject
         public string xscNo { get; set; }
         public string campus { get; set; }
     }
-    
+
     class Staff
     {
         public string staffId { get; set; }
@@ -300,7 +300,68 @@ namespace HandSchool.JLU.JsonObject
             public string menuFile { get; set; }
         }
     }
+    
+    class StudEval
+    {
+        public object personInput { get; set; }
+        public object subject { get; set; }
+        public Target target { get; set; }
+        public string evalItemId { get; set; }
+        public object notes { get; set; }
+        public EvalActTime evalActTime { get; set; }
+        public object dateInput { get; set; }
+        public object targetClar { get; set; }
 
+        public class Target
+        {
+            public School school { get; set; }
+            public string name { get; set; }
+            public string personId { get; set; }
+
+            public class School
+            {
+                public string schoolName { get; set; }
+            }
+        }
+        
+        public class EvalActTime
+        {
+            public string actTimeId { get; set; }
+            public EvalGuideLine evalGuideline { get; set; }
+            public EvalTime evalTime { get; set; }
+            public EvalActivity evalActivity { get; set; }
+        }
+
+        public class EvalGuideLine
+        {
+            public string evalGuidelineId { get; set; }
+            public string paperUrl { get; set; }
+            public string displayMode { get; set; }
+        }
+
+        public class EvalTime
+        {
+            public string title { get; set; }
+            public string phaseType { get; set; }
+            public string activeStatus { get; set; }
+            public string etimeId { get; set; }
+            public Ttl ttl { get; set; }
+        }
+
+        public class Ttl
+        {
+            public string ttlId { get; set; }
+            public DateTime dateStop { get; set; }
+            public string title { get; set; }
+            public DateTime dateStart { get; set; }
+        }
+
+        public class EvalActivity
+        {
+            public string title { get; set; }
+        }
+    }
+    
     class AlreadyKnownThings
     {
         public static NameValueCollection Division { get; } = new NameValueCollection
