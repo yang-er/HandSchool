fcee95c32feabd4e4f29959d86808cf957a64bd2
小羊 <670171961@qq.com>
2018-08-17 14:17:24 +0800

    [A]又一波适配ios

--- HandSchool/HandSchool/Views/IndexPage.xaml.cs
+++ HandSchool/HandSchool/Views/IndexPage.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using HandSchool.ViewModels;
 using System;
+using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace HandSchool.Views
@@ -8,9 +9,14 @@ namespace HandSchool.Views
 	public partial class IndexPage : PopContentPage
 	{
         public IndexPage()
-		{
+        {
             InitializeComponent();
             ViewModel = IndexViewModel.Instance;
+#if __IOS__
+            var themer = new Style(typeof(Frame));
+            themer.Setters.Add(Frame.HasShadowProperty, false);
+            Resources.Add(themer);
+#endif
         }
 
         private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
