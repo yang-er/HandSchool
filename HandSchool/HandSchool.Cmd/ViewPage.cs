e6a0fc542d100ae177cc123c3572bd4ea100c775
小羊 <670171961@qq.com>
2018-08-29 22:42:03 +0800

    [F]修复了HandSchool.Cmd的加载问题

---
+++ HandSchool/HandSchool.Cmd/ViewPage.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using HandSchool.Models;
+using HandSchool.ViewModels;
+using Xamarin.Forms;
+
+namespace HandSchool.Views
+{
+    public class EmptyPage
+    {
+        public Task ShowAsync(INavigation iv = null) => Task.Delay(100);
+    }
+
+    public class LoginPage : EmptyPage
+    {
+        internal LoginPage(LoginViewModel viewModel) { }
+        internal void Response(object sender, LoginStateEventArgs e) { }
+    }
+
+    public class CurriculumPage : EmptyPage
+    {
+        internal CurriculumPage(CurriculumItem a, bool b) { }
+    }
+}
