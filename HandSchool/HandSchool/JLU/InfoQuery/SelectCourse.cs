f2c9b1b072e3f5434c9d7536521717b8d46e39e2
小羊 <670171961@qq.com>
2018-09-06 12:45:24 +0800

    [A]优化选课页面样式

--- HandSchool/HandSchool/JLU/InfoQuery/SelectCourse.cs
+++ HandSchool/HandSchool/JLU/InfoQuery/SelectCourse.cs
@@ -4,13 +4,11 @@ using HandSchool.Models;
 using HandSchool.Services;
 using System;
 using System.Collections.Generic;
-using System.Text;
 using System.Threading.Tasks;
-using Xamarin.Forms;
 
 namespace HandSchool.JLU.InfoQuery
 {
-    [Entrance("快捷选课", "对您备选的选课列表进行操作。", EntranceType.InfoEntrance)]
+    [Entrance("网上选课", "对您备选的选课列表进行操作。", EntranceType.InfoEntrance)]
     [Hotfix("https://raw.githubusercontent.com/yang-er/HandSchool/master/HandSchool/HandSchool/JLU/InfoQuery/selectcourse.js.ver", "jlu_selectcourse.js")]
     class SelectCourse : IInfoEntrance
     {
@@ -58,12 +56,14 @@ namespace HandSchool.JLU.InfoQuery
                 {
                     "<p class=\"mt-3\">目前选课：<span id=\"splanName\">未知</span></p>" +
 
-                    "<table class=\"table\">" +
-                    "<thead><tr><th>状态</th><th>课程</th><th>类型</th></tr></thead><tbody id=\"courList\">" +
+                    "<table class=\"table table-responsive\">" +
+                    "<thead><tr><th style=\"min-width:3.5em\">状态</th><th style=\"min-width:14em\">课程</th>" +
+                    "<th style=\"min-width:5em\">类型</th></tr></thead><tbody id=\"courList\">" +
                     "</tbody></table>" +
 
-                    "<table class=\"table\">" +
-                    "<thead><tr><th>教师</th><th>操作</th><th>时间</th></tr></thead><tbody id=\"schList\">" +
+                    "<table class=\"table table-responsive\">" +
+                    "<thead><tr><th style=\"min-width:5em\">教师</th><th style=\"min-width:4em\">操作</th>" +
+                    "<th style=\"min-width:15em\">时间</th></tr></thead><tbody id=\"schList\">" +
                     "</tbody></table>".ToRawHtml()
                 },
                 JavaScript =
