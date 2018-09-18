0fddc1bb5db765e4278be4f2d5a476c8f0abf609
小羊 <670171961@qq.com>
2018-09-18 21:28:07 +0800

    [A]将消息详情变为可滚动

--- HandSchool/HandSchool/Views/MessageDetailPage.cs
+++ HandSchool/HandSchool/Views/MessageDetailPage.cs
@@ -10,19 +10,25 @@ namespace HandSchool.Views
 		{
             Title = "消息详情";
             ToolbarItems.Add(new ToolbarItem() { Text = "删除", Command = item.Delete });
-            Content = new StackLayout
+
+            Content = new ScrollView
             {
-                Spacing = 10,
-                Padding = new Thickness(20),
-                Children = {
-                    new Label { Text = item.Title, FontSize = 24, TextColor = Color.Black },
-                    new Label { Text = "发件人：" + item.Sender, FontSize = 14 },
-                    new Label { Text = "时间：" + item.Time.ToString(), FontSize = 14 },
-                    new BoxView { Color = Color.Gray, Margin = new Thickness(0,5,0,5), HeightRequest = 1 },
-                    new Label { Text = item.Body, FontSize = 16 }
-                }
+                Content = new StackLayout
+                {
+                    Spacing = 10,
+                    Padding = new Thickness(20),
+                    Children =
+                    {
+                        new Label { Text = item.Title, FontSize = 24, TextColor = Color.Black },
+                        new Label { Text = "发件人：" + item.Sender, FontSize = 14 },
+                        new Label { Text = "时间：" + item.Time.ToString(), FontSize = 14 },
+                        new BoxView { Color = Color.Gray, Margin = new Thickness(0,5,0,5), HeightRequest = 1 },
+                        new Label { Text = item.Body, FontSize = 16 }
+                    }
+                },
+                Orientation = ScrollOrientation.Vertical
             };
-		}
+        }
 
         public MessageDetailPage(FeedItem item)
         {
@@ -30,21 +36,23 @@ namespace HandSchool.Views
             if (item.Link != "") ToolbarItems.Add(new ToolbarItem { Text = "详情", Command = new Command(() => Device.OpenUri(new Uri(item.Link))) });
             var desc = item.Description.Trim();
             while (desc.Contains("    ")) desc = desc.Replace("    ", "  ");
-            Content = new StackLayout
+
+            Content = new ScrollView
             {
-                Spacing = 10,
-                Padding = new Thickness(20),
-                Children = {
-                    new Label { Text = item.Title, FontSize = 24, TextColor = Color.Black },
-                    new Label { Text = "分类：" + item.Category, FontSize = 14 },
-                    new Label { Text = "时间：" + item.PubDate, FontSize = 14 },
-                    new BoxView { Color = Color.Gray, Margin = new Thickness(0,5,0,5), HeightRequest = 1 },
-                    new ScrollView
+                Content = new StackLayout
+                {
+                    Spacing = 10,
+                    Padding = new Thickness(20),
+                    Children = 
                     {
-                        Content = new Label { Text = desc, FontSize = 16, HorizontalOptions = LayoutOptions.FillAndExpand, VerticalOptions = LayoutOptions.FillAndExpand },
-                        Orientation = ScrollOrientation.Vertical
+                        new Label { Text = item.Title, FontSize = 24, TextColor = Color.Black },
+                        new Label { Text = "分类：" + item.Category, FontSize = 14 },
+                        new Label { Text = "时间：" + item.PubDate, FontSize = 14 },
+                        new BoxView { Color = Color.Gray, Margin = new Thickness(0,5,0,5), HeightRequest = 1 },
+                        new Label { Text = desc, FontSize = 16, HorizontalOptions = LayoutOptions.FillAndExpand, VerticalOptions = LayoutOptions.FillAndExpand },
                     }
-                }
+                },
+                Orientation = ScrollOrientation.Vertical
             };
         }
 	}
