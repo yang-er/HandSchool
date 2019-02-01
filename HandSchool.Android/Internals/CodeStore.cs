18b579a560fec69cc3c655b765574b90ee80874f
小羊 <tlylz99@live.cn>
2019-02-02 01:08:00 +0800

    [A]修改了一下目录

---
+++ HandSchool.Android/Internals/CodeStore.cs
@@ -0,0 +1,39 @@
+﻿using HandSchool.Internal;
+using HandSchool.ViewModels;
+using System.Threading.Tasks;
+
+namespace HandSchool.Droid
+{
+    class CodeStore
+    {
+        public void FinishSettings()
+        {
+            Core.App.Service.RequestLogin().ContinueWith(LoginSuccessContinue);
+        }
+
+        private void LoginSuccessContinue(Task<bool> success)
+        {
+            if (success.Result)
+            {
+                if (Core.App.Schedule != null)
+                {
+                    ScheduleViewModel.Instance.RefreshCommand.Execute(null);
+                    if (Core.App.GradePoint != null)
+                    {
+                        ScheduleViewModel.Instance.RefreshComplete += ScheduleComplete;
+                    }
+                }
+                else if (Core.App.GradePoint != null)
+                {
+                    GradePointViewModel.Instance.LoadItemsCommand.Execute(null);
+                }
+            }
+        }
+
+        private void ScheduleComplete()
+        {
+            GradePointViewModel.Instance.LoadItemsCommand.Execute(null);
+            ScheduleViewModel.Instance.RefreshComplete -= ScheduleComplete;
+        }
+    }
+}
