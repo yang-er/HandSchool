ab8a9387364d1432bf45f464073f367d05ce05c5
小羊 <tlylz99@live.cn>
2019-02-12 00:07:44 +0800

    [A]完成win10m build15254的适配

---
+++ HandSchool.UAP/Compact.cs
@@ -0,0 +1,124 @@
+﻿using Microsoft.UI.Xaml.Controls;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Security.Cryptography.Core;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Data;
+
+namespace HandSchool.Internals
+{
+    public class Thread
+    {
+        public int ManagedThreadId { get; set; } = 2;
+
+        public static Thread CurrentThread { get; } = new Thread();
+    }
+
+    public class ServicePointManager
+    {
+        public static Predicate<object> ServerCertificateValidationCallback { get; set; }
+    }
+
+    public class Trace
+    {
+        public static void WriteLine(string ans)
+        {
+            Debug.WriteLine(ans);
+        }
+    }
+
+    public class AwaredWebClientImpl { }
+
+    public interface ICustomAttributeProvider
+    {
+        object[] GetCustomAttributes(Type type, bool inherit);
+    }
+
+    public static class ReflectionExtensionsCompact
+    {
+        public static bool IsAssignableFrom(this Type t1, Type t2)
+        {
+            return TypeExtensions.IsAssignableFrom(t1, t2);
+        }
+
+        public static TAttribute Get<TAttribute>(this Type info, bool @checked = true) where TAttribute : Attribute
+        {
+            return info.GetTypeInfo().Get<TAttribute>(@checked);
+        }
+
+        public static IEnumerable<TAttribute> Gets<TAttribute>(this MemberInfo info) where TAttribute : Attribute
+        {
+            foreach (var obj in info.GetCustomAttributes(typeof(TAttribute), false))
+            {
+                if (obj is TAttribute attr) yield return attr;
+            }
+        }
+
+        public static IEnumerable<TAttribute> Gets<TAttribute>(this Assembly info) where TAttribute : Attribute
+        {
+            foreach (var obj in info.GetCustomAttributes(typeof(TAttribute)))
+            {
+                if (obj is TAttribute attr) yield return attr;
+            }
+        }
+
+        public static bool Has<TAttribute>(this Type info) where TAttribute : Attribute
+        {
+            return info.GetTypeInfo().Has<TAttribute>();
+        }
+
+        public static PropertyInfo[] GetProperties(this Type type)
+        {
+            return TypeExtensions.GetProperties(type);
+        }
+
+        public static MethodInfo[] GetMethods(this Type type)
+        {
+            return TypeExtensions.GetMethods(type);
+        }
+
+        public static MethodInfo GetMethod(this Type type, string methodName)
+        {
+            return TypeExtensions.GetMethod(type, methodName);
+        }
+    }
+
+    public class MD5CryptoServiceProvider : IDisposable
+    {
+        public void Dispose() { }
+
+        public byte[] ComputeHash(byte[] source)
+        {
+            var alg = HashAlgorithmProvider.OpenAlgorithm(HashAlgorithmNames.Md5);
+            var origBuff = WindowsRuntimeBufferExtensions.AsBuffer(source, 0, source.Length);
+            var hashed = alg.HashData(origBuff);
+            return WindowsRuntimeBufferExtensions.ToArray(hashed, 0, (int)hashed.Length);
+        }
+    }
+    
+    public sealed class NavigationViewStateConverter : IValueConverter
+    {
+        Thickness MinimalMargin { get; } = new Thickness(52, -12, 0, -12);
+        Thickness OtherMargin { get; } = new Thickness(12, -12, 0, -12);
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            if (value is NavigationViewDisplayMode _value)
+            {
+                return _value == NavigationViewDisplayMode.Minimal ? MinimalMargin : OtherMargin;
+            }
+            else
+            {
+                throw new InvalidOperationException();
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new InvalidOperationException();
+        }
+    }
+}
---
+++ HandSchool.UAP/MainPage.xaml.cs
@@ -0,0 +1,160 @@
+﻿using HandSchool.Internals;
+using HandSchool.Models;
+using HandSchool.UWP;
+using HandSchool.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Windows.UI.Core;
+using Windows.UI.Xaml;
+using Microsoft.UI.Xaml.Controls;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Controls;
+using NavigationView = Microsoft.UI.Xaml.Controls.NavigationView;
+using NavigationViewItemInvokedEventArgs = Microsoft.UI.Xaml.Controls.NavigationViewItemInvokedEventArgs;
+using NavigationViewItem = Microsoft.UI.Xaml.Controls.NavigationViewItem;
+using Windows.Phone.UI.Input;
+
+namespace HandSchool.Views
+{
+    public sealed partial class MainPage : Page
+    {
+        private bool _isSettingsInvoked = false;
+        
+        private List<NavigationMenuItemImpl> NavMenuItems;
+
+        private static bool winUIresAdded;
+        
+        public MainPage()
+        {
+            if (!winUIresAdded)
+            {
+                App.Current.Resources.MergedDictionaries.Add(new XamlControlsResources());
+                App.Current.Resources.MergedDictionaries.Add(new ResourceDictionary { Source = new Uri("ms-appx:///Compact.xaml") });
+                winUIresAdded = true;
+            }
+
+            InitializeComponent();
+            NavMenuItems = PlatformImpl.Instance.NavigationItems;
+            NavMenuItems.ForEach((i) => NavigationView.MenuItems.Add(i.Value));
+            new NavigateImpl(ContentFrame);
+            ContentFrame.Navigate(typeof(IndexPage));
+            IndexViewModel.Instance.RefreshCommand.Execute(null);
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            SystemNavigationManager.GetForCurrentView().BackRequested += OnBackRequested;
+
+            if ("Windows.Mobile" == Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily)
+            {
+                HardwareButtons.BackPressed += HardwareButtons_BackPressed;
+                MinimalMargin = new Thickness(52, -18, 0, -12);
+                HeaderAreaGrid.FontSize = 16;
+                ContentFrame.Margin = new Thickness(0);
+            }
+
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility = AppViewBackButtonVisibility.Disabled;
+        }
+
+        private void HardwareButtons_BackPressed(object sender, BackPressedEventArgs e)
+        {
+            if (ContentFrame.CanGoBack)
+            {
+                ContentFrame.GoBack();
+                e.Handled = true;
+            }
+        }
+
+        protected override void OnNavigatedFrom(NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+            SystemNavigationManager.GetForCurrentView().BackRequested -= OnBackRequested;
+            if ("Windows.Mobile" == Windows.System.Profile.AnalyticsInfo.VersionInfo.DeviceFamily)
+                HardwareButtons.BackPressed -= HardwareButtons_BackPressed;
+        }
+
+        private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
+        {
+            if (args.InvokedItem is NavigationMenuItemImpl item)
+            {
+                if (item.PageType == null) Core.Logger.WriteLine("UWP", item.Title + " is not finished");
+                if (item.PageType != ContentFrame.CurrentSourcePageType || item.NavigationParameter != currentNavigationParameter)
+                    ContentFrame.Navigate(item.PageType, item.NavigationParameter);
+            }
+            else if (args.IsSettingsInvoked)
+            {
+                if (SettingItem is null)
+                {
+                    var wtf = NavigationView.SettingsItem as NavigationViewItem;
+                    SettingItem = NavigationMenuItemImpl.CreateSettingItem(wtf);
+                    wtf.Content = SettingItem;
+                    NavMenuItems.Add(SettingItem);
+                }
+                
+                if (!_isSettingsInvoked && currentNavigationParameter != SettingItem.NavigationParameter)
+                {
+                    _isSettingsInvoked = true;
+                    ContentFrame.Navigate(SettingItem.PageType, SettingItem.NavigationParameter);
+                    Task.Delay(1000).ContinueWith((task) => _isSettingsInvoked = false);
+                }
+            }
+        }
+
+        NavigationMenuItemImpl SettingItem;
+
+        object currentNavigationParameter;
+
+        private void ContentFrame_Navigated(object sender, NavigationEventArgs e)
+        {
+            SystemNavigationManager.GetForCurrentView().AppViewBackButtonVisibility =
+                ContentFrame.CanGoBack ? AppViewBackButtonVisibility.Visible
+                                       : AppViewBackButtonVisibility.Disabled;
+            currentNavigationParameter = e.Parameter;
+
+            object selected = null;
+            
+            if (e.Content is WebViewPage)
+            {
+                selected = NavMenuItems.Find((item) =>
+                    item.Title == "信息查询"
+                )?.Value;
+            }
+            else if (e.Content is MessageDetailPage)
+            {
+                selected = NavigationView.SelectedItem;
+            }
+            else
+            {
+                selected = NavMenuItems.Find((item) =>
+                    item.PageType == e.SourcePageType &&
+                        item.NavigationParameter == e.Parameter
+                )?.Value;
+            }
+
+            if (NavigationView.SelectedItem != selected)
+                NavigationView.SelectedItem = selected;
+        }
+        
+        private void ContentFrame_Navigating(object sender, NavigatingCancelEventArgs e)
+        {
+            CommandBar?.PrimaryCommands.Clear();
+            CommandBar?.SecondaryCommands.Clear();
+        }
+        
+        private void OnBackRequested(object sender, BackRequestedEventArgs args)
+        {
+            if (ContentFrame.CanGoBack) ContentFrame.GoBack();
+        }
+
+        Thickness MinimalMargin { get; set; } = new Thickness(52, -12, 0, -12);
+        Thickness OtherMargin { get; set; } = new Thickness(12, -12, 0, -12);
+
+        private void NavigationView_DisplayModeChanged(NavigationView sender, Microsoft.UI.Xaml.Controls.NavigationViewDisplayModeChangedEventArgs args)
+        {
+            HeaderAreaGrid.Margin = sender.DisplayMode == Microsoft.UI.Xaml.Controls.NavigationViewDisplayMode.Minimal ? MinimalMargin : OtherMargin;
+        }
+    }
+}
