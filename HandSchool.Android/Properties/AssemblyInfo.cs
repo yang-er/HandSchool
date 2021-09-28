0dc20790cd01de666bcbc88e335d570693fb3a94
ChiChi <dl2021520@gmail.com>
2021-09-28 18:58:54 +0800

    nothing

--- HandSchool.Android/Properties/AssemblyInfo.cs
+++ HandSchool.Android/Properties/AssemblyInfo.cs
@@ -6,6 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+#if DEBUG
+[assembly: Application(Debuggable = true)]
+#else
+[assembly: Application(Debuggable=false)]
+#endif
 [assembly: AssemblyTitle("HandSchool.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
