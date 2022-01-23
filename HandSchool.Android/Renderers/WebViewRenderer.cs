04c5c3337a1052ebacf59bb24af72936a640c3fa
ChiChi <dl2021520@gmail.com>
2022-01-23 20:41:16 +0800

    重写获取Cookie的实现
    和谐OS数据库中的Cookie经过加密，将实现改回CookieManager

--- HandSchool.Android/Renderers/WebViewRenderer.cs
+++ HandSchool.Android/Renderers/WebViewRenderer.cs
@@ -1,16 +1,14 @@
 ﻿using System;
-using System.IO;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using Android.Content;
 using Android.Webkit;
 using HandSchool.Controls;
 using HandSchool.Droid.Renderers;
-using HandSchool.Internals;
 using Xamarin.Forms;
 using Xamarin.Forms.Platform.Android;
 using WebView = Xamarin.Forms.WebView;
-using SQLite;
 using Xamarin.Forms.Internals;
 using Path = System.IO.Path;
 
@@ -18,107 +16,128 @@ using Path = System.IO.Path;
 
 namespace HandSchool.Droid.Renderers
 {
-    [Table("cookies")]
-    internal class AndroidCookieEntity
-    {
-        [Column("host_key")]
-        public string HostKey { get; set; }
-        
-        [Column("name")]
-        public string Name { get; set; }
-        
-        [Column("value")]
-        public string Value { get; set; }
-        
-        [Column("path")]
-        public string Path { get; set; }
-        
-        [Column("expires_utc")]
-        public string ExpiresUtc { get; set; }
-        
-        [Column("is_secure")]
-        public bool IsSecure { get; set; }
-        
-        [Column("is_httponly")]
-        public bool IsHttpOnly { get; set; }
-        
-        [Column("last_access_utc")]
-        public string LastAccessUtc { get; set; }
-        
-        [Column("has_expires")]
-        public bool HasExpires { get; set; }
-        
-        [Column("is_persistent")]
-        public bool IsPersistent { get; set; }
-        
-        [Column("priority")]
-        public string Priority { get; set; }
-        
-        [Column("samesite")]
-        public string SameSite { get; set; }
-        
-        [Column("source_scheme")]
-        public string SourceScheme { get; set; }
-        
-        [Column("creation_utc")]
-        public string CreationUtc { get; set; }
-    }
-
     public class HSWebViewClient : FormsWebViewClient
     {
-        private static string _webViewFilesPath;
         private readonly CookieManager _manager = CookieManager.Instance;
-        private SQLiteTableManager<AndroidCookieEntity> _cookieSql;
 
         public HSWebView WebView { get; set; }
 
         public HSWebViewClient(WebViewRenderer renderer, HSWebView v) : base(renderer)
         {
             WebView = v;
-            TryGetCookies();
         }
 
-        private void TryGetCookies()
-        {
-            if (_cookieSql != null) return;
-            _webViewFilesPath ??= Directory.GetDirectories(BaseActivity.InternalFileRootPath)
-                .Where(d => d.ToLower().Contains("webview"))
-                .FirstOrDefault(d =>
-                {
-                    var current = Path.Combine(BaseActivity.InternalFileRootPath, d);
-                    var dft = Path.Combine(current, "Default");
-                    return Directory.GetDirectories(current).Contains(dft);
-                });
-
-            if (_webViewFilesPath is null) return;
-            _cookieSql = new SQLiteTableManager<AndroidCookieEntity>(false,
-                _webViewFilesPath, "Default", "Cookies");
-        }
-
+        private static string _cookiesDataBase;
         private DateTime? _lastUpdate;
+        
         //默认的Cookie在安卓平台不能正常工作，进行Cookie同步
-        private void SyncCookiesFromStorage()
+        private void SyncCookiesFromStorage(string url)
         {
-            _manager?.Flush();
-            TryGetCookies();
-            if (_cookieSql?.HasTable() != true) return;
-            var updateTime = File.GetLastWriteTime(_cookieSql.DataBasePath);
-            if (_lastUpdate == updateTime) return;
-            _lastUpdate = updateTime;
-            var list = _cookieSql.GetItems();
-            list.Select(ac => new Cookie
+            if (_manager is null) return;
+            if (!_manager.HasCookies) return;
+            _manager.Flush();
+            
+            //首先检测Cookie是否被更新，此步骤可能不可用
+            //因为这是安卓默认的WebView Cookie存放位置，若厂商有自己的WebView实现，则不可用
+            _cookiesDataBase ??= Path.Combine(BaseActivity.InternalFileRootPath, "app_webview", "Default", "Cookies");
+            if (System.IO.File.Exists(_cookiesDataBase))
             {
-                Name = ac.Name,
-                Value = ac.Value,
-                Domain = ac.HostKey,
-                Path = ac.Path,
-                HttpOnly = ac.IsHttpOnly,
-                Secure = ac.IsSecure,
-            }).ForEach(c => WebView?.HSCookies?.Add(c));
+                var update = System.IO.File.GetLastWriteTime(_cookiesDataBase);
+                if (update == _lastUpdate) return;
+                _lastUpdate = update;
+            }
+            
+            var uri = new Uri(url);
+            
+            //将路径拆开
+            var ps = new List<string>{""};
+            uri.AbsolutePath.Trim()
+                .Split('/')
+                .Where(p => !string.IsNullOrWhiteSpace(p))
+                .ForEach(p => ps.Add($"{ps[^1]}/{p}"));
+            
+            //将域名拆开
+            var hosts = uri.Host.Trim().Split('.');
+            var dps = new List<(string, string)>();
+
+            //将所有带有点前缀的父域名加入查询队列
+            var superHost = hosts[^1];
+            for (var i = hosts.Length - 2; i >= 0; i--)
+            {
+                superHost = $"{hosts[i]}.{superHost}";
+                ps.ForEach(p => dps.Add(($".{superHost}", p)));
+            }
+            
+            //将域名本身加入查询队列
+            ps.ForEach(p => dps.Add(($"{uri.Host}", p)));
+            var res = new Dictionary<string, List<Cookie>>();
+            
+            //查询所有的domain-path对，并进行合并处理
+            dps.ForEach(dp =>
+            {
+                var (domain, path) = dp;
+                ParseCookies(
+                    domain, 
+                    path == "" ? "/" : path,
+                    _manager.GetCookie($"{uri.Scheme}://{domain}{path}"))
+                    .ForEach(c => AddCookie(res, c));
+            });
+            
+            //将结果同步到HSCookies
+            res.Values.ForEach(cs => cs.ForEach(WebView.HSCookies.Add));
         }
+
+#nullable enable
+        private static void AddCookie(IDictionary<string, List<Cookie>> dic, Cookie c)
+        {
+            if (dic.ContainsKey(c.Name))
+            {
+                var cur = dic[c.Name];
+                if (!cur.Any(cookie => c.Name == cookie.Name && c.Value == cookie.Value))
+                {
+                    cur.Add(c);
+                }
+            }
+            else
+            {
+                dic[c.Name] = new List<Cookie> {c};
+            }
+        }
+        private static IEnumerable<Cookie> ParseCookies(string domain, string path, string? str)
+        {
+            return str?.Split(";")
+                ?.Select(s => s?.Trim().Split("="))
+                .Where(ss =>
+                {
+                    if (ss is null) return false;
+                    return ss.Length != 0 && ss.Length <= 2;
+                })
+                .Select(ss =>
+                {
+                    var res = new Cookie
+                    {
+                        Domain = domain,
+                        Path = path
+                    };
+                    switch (ss!.Length)
+                    {
+                        case 1:
+                            res.Name = ss[0];
+                            break;
+                        case 2:
+                            res.Name = ss[0];
+                            res.Value = ss[1];
+                            break;
+                    }
+
+                    return res;
+                }) ?? Array.Empty<Cookie>();
+        }
+#nullable disable
+        
         public override void OnPageFinished(Android.Webkit.WebView view, string url)
         {
-            SyncCookiesFromStorage();
+            SyncCookiesFromStorage(url);
             base.OnPageFinished(view, url);
         }
     }
