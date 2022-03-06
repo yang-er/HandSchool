d3589558da3993763f3209edde04e47b27a5ec2d
ChiChi <dl2021520@gmail.com>
2022-03-06 18:19:05 +0800

    Update ViewResponseImpl.cs

--- HandSchool.Android/Internals/ViewResponseImpl.cs
+++ HandSchool.Android/Internals/ViewResponseImpl.cs
@@ -1,5 +1,4 @@
-﻿using Android.App;
-using Android.Content;
+﻿using Android.Content;
 using Android.Graphics;
 using AppActivity = AndroidX.AppCompat.App.AppCompatActivity;
 using Android.Views;
@@ -16,18 +15,17 @@ using HandSchool.Droid.Internals;
 
 namespace HandSchool.Droid
 {
-
     public class ViewResponseImpl : IViewResponseImpl
     {
         public void ReqActAsync(IViewPage sender, ActionSheetArguments args)
         {
             var context = sender.ToContext();
             if (context is null) return;
-            var builder = new MaterialAlertDialogBuilder(context, Resource.Style.MaterialAlertDialog_Rounded); 
+            var builder = new MaterialAlertDialogBuilder(context, Resource.Style.MaterialAlertDialog_Rounded);
             builder.SetTitle(args.Title);
 
             var items = args.Buttons.ToArray();
-            builder.SetItems(items, (o, fargs) => args.Result.TrySetResult(items[fargs.Which]));
+            builder.SetItems(items, (o, selected) => args.Result.TrySetResult(items[selected.Which]));
             if (args.Cancel != null)
                 builder.SetPositiveButton(args.Cancel, (o, e) => args.Result.TrySetResult(args.Cancel));
 
@@ -47,40 +45,34 @@ namespace HandSchool.Droid
             // Create and assign layout
             var context = sender.ToContext();
             if (context is null) return;
-            var builder = new MaterialAlertDialogBuilder(context, Resource.Style.MaterialAlertDialog_Rounded); 
-            LayoutInflater layoutInflater = LayoutInflater.From(sender.ToContext());
-            var chartLayout = layoutInflater.Inflate(Resource.Layout.dialog_chart, null);
+            var builder = new MaterialAlertDialogBuilder(context, Resource.Style.MaterialAlertDialog_Rounded);
+
+            var chartLayout = View.Inflate(context, Resource.Layout.dialog_chart, null);
             builder.SetView(chartLayout);
             builder.SetTitle(args.Title);
-            builder.SetPositiveButton(args.Close, (IDialogInterfaceOnClickListener)null);
+            builder.SetPositiveButton(args.Close, listener: null);
 
             // Set drawing content
             args.Chart.LabelTextSize = Core.Platform.Dip2Px(12);
             var canvasView = chartLayout.FindViewById<SKCanvasView>(Resource.Id.skia_chart_canvas);
-            canvasView.PaintSurface += (s, e) =>
-            {
-                args.Chart.Draw(e.Surface.Canvas, e.Info.Width, e.Info.Height);
-            };
+            canvasView.PaintSurface += (s, e) => { args.Chart.Draw(e.Surface.Canvas, e.Info.Width, e.Info.Height); };
 
             // Show and fix position
             var dialog = builder.Create();
             dialog.DismissEvent += (s, e) => args.ReturnTask.Start();
             dialog.Show();
 
-            IWindowManager manager = ((BaseActivity)sender.ToContext()).WindowManager;
-            Display d = manager.DefaultDisplay;
-            Window window = dialog.Window;
-            WindowManagerLayoutParams param = window.Attributes;
-            param.Height = Core.Platform.Dip2Px(340);
-            param.Gravity = GravityFlags.CenterHorizontal;
-            dialog.Window.Attributes = param;
+            var attr = dialog.Window.Attributes;
+            attr.Height = Core.Platform.Dip2Px(430);
+            attr.Gravity = GravityFlags.CenterHorizontal;
+            dialog.Window.Attributes = attr;
         }
 
         public void ReqInpAsync(IViewPage sender, RequestInputArguments args)
         {
             var context = sender.ToContext();
             if (context is null) return;
-            var builder = new MaterialAlertDialogBuilder(context, Resource.Style.MaterialAlertDialog_Rounded); 
+            var builder = new MaterialAlertDialogBuilder(context, Resource.Style.MaterialAlertDialog_Rounded);
             string answer = null;
 
             LayoutInflater layoutInflater = LayoutInflater.From(sender.ToContext());
@@ -93,19 +85,16 @@ namespace HandSchool.Droid
 
             builder.SetTitle(args.Title);
             builder.SetPositiveButton(args.Accept, (s, e) => answer = textBox.Text);
-            builder.SetNegativeButton(args.Cancel, (IDialogInterfaceOnClickListener)null);
+            builder.SetNegativeButton(args.Cancel, (IDialogInterfaceOnClickListener) null);
             builder.SetCancelable(false);
 
             var dialog = builder.Create();
 
-            dialog.DismissEvent += (s, e) =>
-            {
-                args.SetResult(answer);
-            };
+            dialog.DismissEvent += (s, e) => { args.SetResult(answer); };
 
             dialog.ShowEvent += (s, e) =>
             {
-                var imm = (InputMethodManager)sender.ToContext().GetSystemService(Context.InputMethodService);
+                var imm = (InputMethodManager) sender.ToContext().GetSystemService(Context.InputMethodService);
                 imm.ShowSoftInput(textBox, ShowFlags.Forced);
             };
 
@@ -116,19 +105,19 @@ namespace HandSchool.Droid
         {
             var context = sender.ToContext();
             if (context is null) return;
-            var builder = new MaterialAlertDialogBuilder(context, Resource.Style.MaterialAlertDialog_Rounded); 
+            var builder = new MaterialAlertDialogBuilder(context, Resource.Style.MaterialAlertDialog_Rounded);
             string answer = null;
 
-            var image_input = View.Inflate(context, Resource.Layout.ask_for_code, null) as LinearLayout;
-            builder.SetView(image_input);
+            var imageInput = View.Inflate(context, Resource.Layout.ask_for_code, null) as LinearLayout;
+            builder.SetView(imageInput);
 
-            var msg = image_input.FindViewById(Resource.Id.askcode_msg) as TextView;
+            var msg = imageInput.FindViewById(Resource.Id.askcode_msg) as TextView;
             msg.Text = args.Message;
 
-            var img = image_input.FindViewById(Resource.Id.code_img) as ImageView;
+            var img = imageInput.FindViewById(Resource.Id.code_img) as ImageView;
             img.SetImageBitmap(BitmapFactory.DecodeByteArray(args.Sources, 0, args.Sources.Length));
 
-            var edit = image_input.FindViewById(Resource.Id.code_input) as EditText;
+            var edit = imageInput.FindViewById(Resource.Id.code_input) as EditText;
             builder.SetTitle(args.Title);
             builder.SetPositiveButton(args.Accept, (s, e) => answer = edit.Text);
             builder.SetNegativeButton(args.Cancel, listener: null);
@@ -136,14 +125,11 @@ namespace HandSchool.Droid
 
             var dialog = builder.Create();
 
-            dialog.DismissEvent += (s, e) =>
-            {
-                args.SetResult(answer);
-            };
+            dialog.DismissEvent += (s, e) => { args.SetResult(answer); };
 
             dialog.ShowEvent += (s, e) =>
             {
-                var imm = (InputMethodManager)sender.ToContext().GetSystemService(Context.InputMethodService);
+                var imm = (InputMethodManager) sender.ToContext().GetSystemService(Context.InputMethodService);
                 imm.ShowSoftInput(edit, ShowFlags.Forced);
             };
 
@@ -154,54 +140,58 @@ namespace HandSchool.Droid
         {
             var context = sender.ToContext();
             if (context is null) return;
-            var alert = new MaterialAlertDialogBuilder(context, Resource.Style.MaterialAlertDialog_Rounded).Create(); 
+            var alert = new MaterialAlertDialogBuilder(context, Resource.Style.MaterialAlertDialog_Rounded).Create();
             alert.SetTitle(args.Title);
             alert.SetMessage(args.Message);
 
             if (args.Accept != null)
-                alert.SetButton((int)DialogButtonType.Positive, args.Accept, (o, e) => args.SetResult(true));
+                alert.SetButton((int) DialogButtonType.Positive, args.Accept, (o, e) => args.SetResult(true));
 
-            alert.SetButton((int)DialogButtonType.Negative, args.Cancel, (o, e) => args.SetResult(false));
+            alert.SetButton((int) DialogButtonType.Negative, args.Cancel, (o, e) => args.SetResult(false));
             alert.CancelEvent += (o, e) => args.SetResult(false);
             alert.Show();
         }
-        
-        public void ReqWebDiaAsync(IViewPage sender, RequestWebDialogArguments args, WebDialogAdditionalArgs additionalArgs)
+
+        public void ReqWebDiaAsync(IViewPage sender, RequestWebDialogArguments args,
+            WebDialogAdditionalArgs additionalArgs)
         {
             var context = sender.ToContext();
             if (context is null) return;
-            var builder = new MaterialAlertDialogBuilder(context, Resource.Style.MaterialAlertDialog_Rounded); 
+            var builder = new MaterialAlertDialogBuilder(context, Resource.Style.MaterialAlertDialog_Rounded);
             string answer = null;
 
             //获取页面
-            var webView = View.Inflate(context, Resource.Layout.web_dialog, null) as LinearLayout;
-            builder.SetView(webView);
-            
+            var linearLayout = View.Inflate(context, Resource.Layout.web_dialog, null) as LinearLayout;
+            builder.SetView(linearLayout);
 
-            var msg = webView.FindViewById(Resource.Id.web_dia_text) as TextView;
-            if (args.Message == null || args.Message.Length == 0)
+
+            var msg = linearLayout.FindViewById<TextView>(Resource.Id.web_dia_text);
+            if (args.Message.IsBlank())
                 msg.Visibility = ViewStates.Gone;
             msg.Text = args.Message;
 
             //找到WebView
-            var browser = webView.FindViewById(Resource.Id.web_dia_browser) as WebView;
-            browser.LoadUrl(args.Url);
-            browser.Settings.MixedContentMode = MixedContentHandling.AlwaysAllow;
-            browser.Settings.JavaScriptEnabled = true;
-            
+            var webView = linearLayout.FindViewById<WebView>(Resource.Id.web_dia_browser);
+            webView.LoadUrl(args.Url);
+            webView.Settings.MixedContentMode = MixedContentHandling.AlwaysAllow;
+            webView.Settings.JavaScriptEnabled = true;
+
             if (additionalArgs != null)
             {
-                var addArgs = (AndroidWebDialogAdditionalArgs)additionalArgs;
-                browser.SetWebChromeClient(addArgs.WebChromeClient);
-                browser.SetWebViewClient(addArgs.WebViewClient);
-                var cm = CookieManager.Instance;
-                foreach(var item in addArgs.Cookies)
+                var addArgs = (AndroidWebDialogAdditionalArgs) additionalArgs;
+                webView.SetWebChromeClient(addArgs.WebChromeClient);
+                webView.SetWebViewClient(addArgs.WebViewClient);
+                addArgs.Cookies?.Let(cookies =>
                 {
-                    var pre = args.Url.ToLower().StartsWith("https://") ? "https://" : "http://";
-                    cm.SetCookie(pre + item.Domain + item.Path, item.ToString());
-                }
+                    foreach (var item in addArgs.Cookies)
+                    {
+                        var pre = args.Url.ToLower().StartsWith("https://") ? "https://" : "http://";
+                        CookieManager.Instance!.SetCookie(pre + item.Domain + item.Path, item.ToString());
+                    }
+                });
             }
-            var edit = webView.FindViewById(Resource.Id.web_dia_resp) as EditText;
+
+            var edit = linearLayout.FindViewById<EditText>(Resource.Id.web_dia_resp);
             if (!args.HasInput)
             {
                 edit.Visibility = ViewStates.Gone;
@@ -211,61 +201,55 @@ namespace HandSchool.Droid
                 edit.Hint = args.InputHint;
             }
 
-            var url = webView.FindViewById(Resource.Id.web_dia_url_text) as TextView;
+            var url = linearLayout.FindViewById<TextView>(Resource.Id.web_dia_url_text);
             url.Text = "此页面由" + args.Url + "提供";
 
-            var goback = webView.FindViewById(Resource.Id.web_dia_goback) as Button;
-            var goforward = webView.FindViewById(Resource.Id.web_dia_goforward) as Button;
-            var relod = webView.FindViewById(Resource.Id.web_dia_reload) as ImageButton;
-            relod.Click += (s, e) =>
-            {
-                var web = ((View)((View)s).Parent.Parent).FindViewById(Resource.Id.web_dia_browser) as WebView;
-                web.Reload();
-            };
+            var goBack = linearLayout.FindViewById<Button>(Resource.Id.web_dia_goback);
+            var goForward = linearLayout.FindViewById<Button>(Resource.Id.web_dia_goforward);
+            var reload = linearLayout.FindViewById<ImageButton>(Resource.Id.web_dia_reload);
+            reload.Click += (s, e) => { webView.Reload(); };
             if (args.Navigation)
             {
-                goback.Click += (s, e) =>
+                goBack.Click += (s, e) =>
                 {
-                    var web = ((View)((Button)s).Parent.Parent).FindViewById(Resource.Id.web_dia_browser) as WebView;
-                    if (web.CanGoBack()) web.GoBack();
+                    if (webView.CanGoBack()) webView.GoBack();
                 };
-                goforward.Click += (s, e) =>
+                goForward.Click += (s, e) =>
                 {
-                    var web = ((View)((Button)s).Parent.Parent).FindViewById(Resource.Id.web_dia_browser) as WebView;
-                    if (web.CanGoForward()) web.GoForward();
+                    if (webView.CanGoForward()) webView.GoForward();
                 };
-                
             }
             else
             {
-                relod.Visibility = ViewStates.Visible;
-                goforward.Visibility = goback.Visibility = ViewStates.Gone;
+                reload.Visibility = ViewStates.Visible;
+                goForward.Visibility = goBack.Visibility = ViewStates.Gone;
             }
+
             builder.SetTitle(args.Title);
-            if (args.Accept != null && args.Accept != "")
+            if (args.Accept.IsNotBlank())
             {
                 builder.SetPositiveButton(args.Accept, (s, e) =>
                 {
                     if (args.HasInput) answer = edit.Text;
-                    browser.LoadUrl("about:blank");
-                    browser.Dispose();
+                    webView.LoadUrl("about:blank");
+                    webView.Dispose();
                 });
             }
-            if (args.Cancel != null && args.Cancel != "")
+
+            if (args.Cancel.IsNotBlank())
             {
-                builder.SetNegativeButton(args.Cancel, (s,e)=> { 
-                    browser.LoadUrl("about:blank");
-                    browser.Dispose(); 
+                builder.SetNegativeButton(args.Cancel, (s, e) =>
+                {
+                    webView.LoadUrl("about:blank");
+                    webView.Dispose();
                 });
             }
+
             builder.SetCancelable(false);
 
             var dialog = builder.Create();
 
-            dialog.DismissEvent += (s, e) =>
-            {
-                args.SetResult(answer);
-            };
+            dialog.DismissEvent += (s, e) => { args.SetResult(answer); };
             dialog.Show();
         }
     }
