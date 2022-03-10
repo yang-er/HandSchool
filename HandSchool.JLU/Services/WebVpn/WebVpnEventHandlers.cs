018b479683fecf1901016b29c7573c82e937f604
ChiChi <dl2021520@gmail.com>
2022-03-10 23:32:42 +0800

    移除vpn登录界面的杂项

--- HandSchool.JLU/Services/WebVpn/WebVpnEventHandlers.cs
+++ HandSchool.JLU/Services/WebVpn/WebVpnEventHandlers.cs
@@ -42,14 +42,22 @@ namespace HandSchool.JLU.Services
             if (e.Url == "https://webvpn.jlu.edu.cn/login")
             {
                 await Events.WebViewEvents.EvaluateJavaScriptAsync(
-                    "document.getElementsByName('remember_cookie')[0].checked=true; " +
-                    "document.getElementsByClassName('remember-field')[0].hidden=true; " + 
+                    "try {" +
+                    "$('[name=\"remember_cookie\"]').attr('checked', true); " +
+                    "jQuery('.remember-field').attr('hidden', true); " +
+                    "document.getElementsByClassName('login-panel')[0].style.borderRadius='15px'; " +
+                    "jQuery('.mobile-notice-panel,.notice-panel').remove(); " +
+                    "$('#copyright-ql-editor').remove(); " +
+                    "}catch {}");
+
+                await Events.WebViewEvents.EvaluateJavaScriptAsync(
                     "$('#login').click(function(){ " +
                     "let uid = document.getElementById('user_name').value; " +
                     "let pwd = document.getElementsByName('password')[0].value; " +
                     $"let encryptedPwd = encrypt(pwd, '{PasswordEncryptKey}', '{PasswordEncryptKey}'); " +
                     $"invokeNativeAction(uid + ' ' + pwd + ' ' + encryptedPwd); " +
                     "})");
+
                 if (!string.IsNullOrWhiteSpace(Username))
                 {
                     await Events.WebViewEvents.EvaluateJavaScriptAsync(
