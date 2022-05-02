0deace497b7e63515e592690248f12044524859b
ChiChi <dl2021520@gmail.com>
2022-05-02 12:16:44 +0800

    适配教评

--- HandSchool.JLU/Models/EvaItemShell.cs
+++ HandSchool.JLU/Models/EvaItemShell.cs
@@ -5,25 +5,31 @@ namespace HandSchool.JLU.Models
     public class EvaItemShell
     {
         public StudEval InnerInfo { get; }
+
         public EvaItemShell(StudEval studEval, bool isEnable)
         {
             InnerInfo = studEval;
             IsEnable = isEnable;
         }
+
         public bool IsEnable { get; set; }
-        
+
         public string TeacherName => InnerInfo?.target?.name;
         public string TeachCourse => InnerInfo?.targetClar?.notes;
 
         public bool IsEvaluated => !string.IsNullOrWhiteSpace(InnerInfo?.dateInput);
+
+        public string EvalGuidelineId => InnerInfo?.evalActTime?.evalGuideline?.evalGuidelineId;
+
         public Xamarin.Forms.Color Color
         {
             get
             {
                 if (!IsEnable) return Xamarin.Forms.Color.Black;
-                return IsEvaluated?  Xamarin.Forms.Color.Gray : Xamarin.Forms.Color.Red;
+                return IsEvaluated ? Xamarin.Forms.Color.Gray : Xamarin.Forms.Color.Red;
             }
         }
+
         public string Detail
         {
             get
--- HandSchool.JLU/ViewModels/TeacherEvaVM.cs
+++ HandSchool.JLU/ViewModels/TeacherEvaVM.cs
@@ -17,7 +17,7 @@ namespace HandSchool.JLU.ViewModels
 { 
     public class TeacherEvaVM : NoticeCheckViewModel
     {
-        public const string TemplateVersion = "180";
+        public static readonly IReadOnlyCollection<string> SupportTemplateVersions = new HashSet<string> { "180", "190" };
         public static TeacherEvaVM Instance => lazy.Value;
         private static Lazy<TeacherEvaVM> lazy = new Lazy<TeacherEvaVM>(() => new TeacherEvaVM());
         private const string GetStuInfoUrl = "action/getCurrentUserInfo.do";
@@ -68,7 +68,7 @@ namespace HandSchool.JLU.ViewModels
 
                 var items = JsonConvert.DeserializeObject<JObject>(str)?["value"]?.ToObject<List<StudEval>>()
                     ?.Select(e => new EvaItemShell(e,
-                        e.evalActTime?.evalGuideline?.evalGuidelineId?.Equals(TemplateVersion) == true));
+                        SupportTemplateVersions.Contains(e.evalActTime?.evalGuideline?.evalGuidelineId)));
                 Core.Platform.EnsureOnMainThread(() =>
                 {
                     EvaItems.Clear();
@@ -90,21 +90,22 @@ namespace HandSchool.JLU.ViewModels
         {
             try
             {
-                _classMates ??= await GetClassMates();
-                if (evaItem?.InnerInfo is null) return TaskResp.False;
-                var getPuzzle = "{\"evalItemId\":\"" + evaItem.InnerInfo.evalItemId + "\"}";
-                var res = await Core.App.Service.Post(PuzzleUrl, getPuzzle);
-                var jo = JsonConvert.DeserializeObject<JObject>(res)?["items"]?[0]?["puzzle"]?.ToString()
-                    .Replace("_", ".");
-                if (string.IsNullOrWhiteSpace(jo)) return TaskResp.False;
-                if (jo.Contains("."))
+                switch (evaItem.EvalGuidelineId)
                 {
-                    foreach (var classMate in _classMates)
+                    case "180":
                     {
-                        var m = Regex.Match(classMate, jo);
-                        if (m.Success && m.Length == jo.Length)
+                        _classMates ??= await GetClassMates();
+                        if (evaItem?.InnerInfo is null) return TaskResp.False;
+                        var getPuzzle = "{\"evalItemId\":\"" + evaItem.InnerInfo.evalItemId + "\"}";
+                        var res = await Core.App.Service.Post(PuzzleUrl, getPuzzle);
+                        var jo = JsonConvert.DeserializeObject<JObject>(res)?["items"]?[0]?["puzzle"]?.ToString()
+                            .Replace("_", ".");
+                        if (string.IsNullOrWhiteSpace(jo)) return TaskResp.False;
+                        if (!jo.Contains(".")) return false;
+                        foreach (var classMate in _classMates)
                         {
-
+                            var m = Regex.Match(classMate, jo);
+                            if (!m.Success || m.Length != jo.Length) continue;
                             var index = jo.IndexOf(".", StringComparison.Ordinal);
                             var ans = m.Value[index];
                             var postValue = "{\"evalItemId\":\"" + evaItem.InnerInfo.evalItemId +
@@ -114,13 +115,24 @@ namespace HandSchool.JLU.ViewModels
                             var resp = JsonConvert.DeserializeObject<JObject>(evaRes)?["status"]?.ToString();
                             return resp == "0" ? TaskResp.True : TaskResp.False;
                         }
+
+                        return false;
                     }
+                    case "190":
+                    {
+                        var postValue = "{\"evalItemId\":\"" + evaItem.InnerInfo.evalItemId +
+                                        "\",\"answers\":{\"p01\":\"A\",\"p02\":\"A\",\"p03\":\"A\",\"p04\":\"A\",\"p05\":\"A\",\"p06\":\"A\",\"p07\":\"A\",\"p08\":\"A\",\"p09\":\"A\",\"p10\":\"A\",\"sat11\":\"A\",\"sat12\":\"A\",\"sat13\":\"A\"}}";
+                        var evaRes = await Core.App.Service.Post(EvaUrl, postValue);
+                        var resp = JsonConvert.DeserializeObject<JObject>(evaRes)?["status"]?.ToString();
+                        return resp == "0";
+                    }
+                    default:
+                        return false;
                 }
-                return TaskResp.False;
             }
             catch (Exception e)
             {
-                return TaskResp.False;
+                return false;
             }
         }
 
@@ -128,7 +140,8 @@ namespace HandSchool.JLU.ViewModels
         public async Task EvalAll()
         {
             if(IsBusy) return;
-            if(EvaItems.Count == 0 || EvaItems.All(e => !e.IsEnable || e.IsEvaluated))
+            var queue = EvaItems.Where(i => !i.IsEvaluated && i.IsEnable).ToArray();
+            if (queue.Length == 0)
             {
                 await RequestMessageAsync("提示", "未发现可评老师");
                 return;
@@ -138,7 +151,7 @@ namespace HandSchool.JLU.ViewModels
             try
             {
                 IsBusy = true;
-                foreach (var e in EvaItems)
+                foreach (var e in queue)
                 {
                     if ((await EvalOne(e)).IsSuccess)
                     {
